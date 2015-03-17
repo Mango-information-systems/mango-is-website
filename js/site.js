515c9a421e99090bdbcbdfefd11d5eed419fbdd2
Mehdi El Fadil <m.ef@mango-is.com>
2015-03-17 18:51:26 +0100

    added site version custom dimension tracking google analytics

--- js/site.js
+++ js/site.js
@@ -49,6 +49,7 @@ var $body
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga')
 
 	ga('create', '{{ site.ga }}', 'auto')
+	ga('set', 'dimension1', '{{ site.version }}')
 	toGa('pageview')
 	
 	// page views, exceptions and events tracking
