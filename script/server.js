48db3d4203014221906029239b43c0887ae0f7f2
Mehdi El Fadil <m.ef@mango-is.com>
2016-06-05 00:38:16 +0200

    continued #49 wip setup node-persist

--- script/server.js
+++ script/server.js
@@ -1,5 +1,9 @@
 var io = require('socket.io')()
+	, storage = require('node-persist')
 	, debug = require('debug')('server')
+
+
+storage.init()
 
 io.on('connection', function(socket) {
 	
@@ -15,8 +19,21 @@ io.on('connection', function(socket) {
 		debug('contact request received', socket.id, formData)
 		
 		// sanitize data then honeypot check then send to contact controller
+
+		//~ honeyPot(formData, sanitizeInput)
+
+		// then run someth
+		//~ sanitizeInput(formData, 
 		
+		var messages = storage.getItem(socket.id) || []
 		
+		formData.ts = new Date()
+		
+		messages.push(formData)
+		
+		storage.setItem(socket.id, messages)
+		
+		// sample response
 		callback({status:403})
 	})
 
