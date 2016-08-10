cd2685017060175fdb91b78d571dc16a7fdee1b8
Mehdi El Fadil <m.ef@mango-is.com>
2016-08-10 15:52:53 +0200

    deployment scripts tuning * differentiated test and prod deployment tasks * dynamic server common to both environments at the moment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,14 @@
-sudo cp -rp public/* /var/www/mango-is/
-sudo chown -R www-data:www-data /var/www/mango-is
-sudo cp -rp dynamic/* /home/srv-node-mango/
-sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango
-sudo systemctl restart node-mango-is
+if [ -z ${1} ];
+then
+	echo "environment is unset, exiting";
+	
+else
+	echo "deploying to '$1' environment";
+	sudo cp -rp public/* /var/www/$1.mango-is.com/
+	sudo chown -R www-data:www-data /var/www/$1.mango-is.com
+	sudo cp -rp dynamic/* /home/srv-node-mango/app
+	sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango/app
+	sudo systemctl restart node-mango-is
+fi
+
+
