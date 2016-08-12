40e1815601c711faa1a7573ab79a1ca143053273
Mehdi El Fadil <m.ef@mango-is.com>
2016-08-12 14:57:41 +0200

    added deployment task for robots.txt

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,10 @@
 if [ -z ${1} ];
 then
 	echo "environment is unset, exiting";
-	
 else
 	echo "deploying to '$1' environment";
 	sudo cp -rp public/* /var/www/$1.mango-is.com/
+	sudo cp -rp config/$1.robots.txt /var/www/$1.mango-is.com/robots.txt
 	sudo chown -R www-data:www-data /var/www/$1.mango-is.com
 	sudo cp -rp dynamic/* /home/srv-node-mango/app
 	sudo chown -R srv-node-mango:srv-node-mango /home/srv-node-mango/app
