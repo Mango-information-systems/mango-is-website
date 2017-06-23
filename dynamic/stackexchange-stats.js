c18cf7880bbd31b2cf550d069afa5d96b902f286
Mehdi El Fadil <m.ef@mango-is.com>
2017-06-23 22:53:42 +0200

    wip starting #96 creation of the app's backbone

---
+++ dynamic/stackexchange-stats.js
@@ -0,0 +1,233 @@
+window.appDebug = require('debug')
+
+var d3 = require('d3')
+	, debug = window.appDebug('stackexchange-stats')
+	, AnalyticsApi = require('./controller/analytics-api')
+	, app = {
+		controller: {}
+		, view: {
+			signIn: require('./view/sign-in-with-google')
+			, dashboard: require('./view/dashboard')
+			, cookieWarning: require('./view/cookie-warning')
+			, donuts: require('./view/donuts')
+		}
+		, data: {}
+	}
+
+var appContainer = d3.select('#app')
+	, dashboardBody
+	, refreshInterval
+
+/******************************************
+ * 
+ * Inline controllers
+ * 
+ * ***************************************/
+ 
+ 
+/**
+* update charts whenever new data is received from Google Analytics API
+* 
+*/
+app.controller.updateStatsCharts = function (stats){
+	
+	// update visitorsCount metrics values
+	stats.forEach(function(stat) {
+		app.viewMap[stat.viewId].value = stat.value
+	})
+	
+	var account = app.data[app.data.currentAccountIndex]
+		
+	// update maxValue for each account
+	var maxValue = setMaxvalue(account)
+	
+	account.webProperties.forEach(function(property, i) {
+		
+		property.maxValue = maxValue
+		
+	})
+	
+	app.view.donuts.update(account.webProperties)
+	
+}
+
+/**
+* Switch monitored account
+* 
+*/	
+app.controller.switchAccount = function(ix) {
+	
+	debug('switching account', ix)
+
+	app.data.currentAccountIndex = ix
+	
+	app.view.donuts.render({
+		target: dashboardBody
+		, properties: app.data[app.data.currentAccountIndex].webProperties
+	})
+	
+	app.controller.analyticsApi.getStats(app.data.viewsByAccount[ix])
+	
+}
+
+
+/******************************************
+ * 
+ * End inline controllers //
+ * 
+ * ***************************************/
+ 
+
+
+/**
+* initialize analytics API controller once the googgle Analytics javascript client library script is loadeed
+* 
+*/
+window.gApiLoaded = function() {
+	
+	app.controller.analyticsApi = new AnalyticsApi(gapi, start, app.controller.updateStatsCharts)
+
+}
+
+/**
+* Display login form or data, depending on user's connection status
+* 
+*/
+function start(err) {
+
+	if (err && err.error == 'idpiframe_initialization_failed') {
+	// third-party cookies are disabled (or something)
+		// https://github.com/google/google-api-javascript-client/issues/260#issuecomment-278514289
+		app.view.cookieWarning.render({target: appContainer })
+	}
+	else if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
+	// user has not connected his Google Analytics account yet
+
+		app.view.signIn.render({
+			target: appContainer
+			, hasError: typeof err !== 'undefined'
+			, action: function() {
+				app.controller.analyticsApi.signIn(start)
+			}
+		})
+	}
+	else {
+	// user is already logged-in
+	
+		getViews()
+
+	}
+}
+
+/**
+* 
+* 
+*/
+function handleSigninResponse(err) {
+	if (err)
+		start(true)
+	else
+		getViews()
+}
+
+/**
+* Render dashboard and initiate data refresh process
+* 
+*/
+function getViews() {
+	
+	app.controller.analyticsApi.getViews(function(res) {
+		
+		app.data = res.result.items
+		
+		app.data.viewsByAccount = {}
+		
+		// pointer to each view in the hierarchy, by Id
+		app.viewMap = {}
+		
+		// use the first account by default
+		app.data.currentAccountIndex = 0
+		
+		
+		// store the view Ids for metrics queries
+		app.data.forEach(function(account, i) {
+
+			app.data.viewsByAccount[i] = []
+			
+			account.webProperties.forEach(function(property) {
+			
+				property.profiles.forEach(function(view) {
+					
+					app.data.viewsByAccount[i].push(view.id)
+					
+					app.viewMap[view.id] = view
+					
+					view.value = 0
+				})
+				
+				property.maxValue = 0
+				
+			})
+		})
+		
+		app.view.dashboard.render({
+			target: appContainer
+			, data: app.data
+			, logOutFn: function() {
+				app.controller.analyticsApi.signOut(start)
+				clearInterval(refreshInterval)
+			}
+			, selectFn: function(ix) {
+				//~ app.view.donuts[property.id].reset()
+				app.controller.switchAccount(ix)
+				
+			}
+		}, function() {
+			// dashboard initial rendering is done
+			
+			dashboardBody = d3.select('#dashboardBody')
+			
+			app.view.donuts.render({
+				target: dashboardBody
+				, properties: app.data[app.data.currentAccountIndex].webProperties
+			})
+			
+		})
+	
+		// retrieve views metrics
+		app.controller.analyticsApi.getStats(app.data.viewsByAccount[app.data.currentAccountIndex])
+		
+		// periodically refresh metrics
+		refreshInterval = setInterval(function() {
+			
+			app.controller.analyticsApi.getStats(app.data.viewsByAccount[app.data.currentAccountIndex])
+				
+		}, 10000)
+		
+	})
+	
+	
+}
+
+
+
+/**
+* compute and return highest metric value across all properties of a given acount
+* 
+*/
+function setMaxvalue(account) {
+	
+	var maxValue = 0
+				
+	account.webProperties.forEach(function(property, i) {
+		
+		property.profiles.forEach(function(view) {
+			
+			if (+view.value > +maxValue)
+				maxValue = view.value
+		})
+	})
+	
+	return maxValue
+	
+}
---
+++ source/tools/stackexchange-tags-dataviz/index.md
@@ -0,0 +1,53 @@
+---
+layout: landing
+title: stackExchange tags visualization
+author: Mango Information Systems
+date: 2016-10-24 16:29:26
+description: Showcase your stackExchange tags in a neat data visualization. Works on stackOverflow as well as the other stackExchange network sites.
+thumbnail: img/thumbnails/stackexchange-tags.png
+css: [stackexchange-tags.css]
+scripts: [js/stackexchange-tags.js, https://api.stackexchange.com/js/2.0/all.js]
+__index: true
+sections:
+ -
+  # top section: title
+  content:
+   - type: h1
+     class: small
+     value: StackExchange tags data visualization
+   - type: h2
+     class: small
+     value: Showcase your stackExchange tags stats in a neat data visualization.
+ -
+  # app
+  content:
+   - type: div
+     id: app
+     value: "<?xml version='1.0' encoding='utf-8'?><svg width='120px' height='120px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid' class='uil-ripple'><rect x='0' y='0' width='100' height='100' fill='none' class='bk'></rect><g> <animate attributeName='opacity' dur='2s' repeatCount='indefinite' begin='0s' keyTimes='0;0.33;1' values='1;1;0'></animate><circle cx='50' cy='50' r='40' stroke='#565656' fill='none' stroke-width='6' stroke-linecap='round'><animate attributeName='r' dur='2s' repeatCount='indefinite' begin='0s' keyTimes='0;0.33;1' values='0;22;44'></animate></circle></g><g><animate attributeName='opacity' dur='2s' repeatCount='indefinite' begin='1s' keyTimes='0;0.33;1' values='1;1;0'></animate><circle cx='50' cy='50' r='40' stroke='#FF6600' fill='none' stroke-width='6' stroke-linecap='round'><animate attributeName='r' dur='2s' repeatCount='indefinite' begin='1s' keyTimes='0;0.33;1' values='0;22;44'></animate></circle></g></svg><br><small>Loading...</small>"
+ -
+  # How it works
+  content:
+   - type: h3
+     value: How it works
+   - type: ol
+     value: "<li>Connect your stackExchange account</li>
+		<li>Visualize statistics about the tags you interact the most with on stackExchange</li>
+		<li>Use the visualization in your own website, or share on social media...</li>"
+ -
+  # About
+  content:
+   - type: h3
+     value: About
+   - type: p
+     value: This tool lets you visualize [...].
+   - type: p
+     value: It is useful when you need to [...].
+   - type: h4
+     value: Privacy friendly
+   - type: p
+     value: The websites traffic data coming from stackExchange transits directly from stackExchange to your browser screen, without ever passing through our servers.
+   - type: h4
+     value: Open source
+   - type: p
+     value: This tool is open source, dual-licensed under MIT and LGPL. You can find the source code of this app <a href="https://github.com/Mango-information-systems/mango-is-website/">here</a>.
+---
