359e5f4f954b47b490ed6759105f5d66909405cf
Mehdi El Fadil <m.ef@mango-is.com>
2017-06-26 11:31:05 +0200

    #96 tweaked force layout settings results in more consistent clustering of the graph nodes TODO code cleanup, further tuning, cleanup layout

--- dynamic/view/force.js
+++ dynamic/view/force.js
@@ -15,6 +15,8 @@ function ForceChart() {
 	var textScale = d3.scaleLinear()
 		  .range([1, 3])
 		, color = d3.scaleOrdinal(d3.schemeCategory10)
+		, r = d3.scaleLinear()
+		  .range([3, 15])
 
 	/****************************************
 	 *
@@ -34,14 +36,15 @@ function ForceChart() {
 		 
 		  //~ self.node.attr('cx', function(d) { return d.x })
 			  //~ .attr('cy', function(d) { return d.y })
+		self.node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-		  self.node.attr('x', function(d) { return d.x })
-			  .attr('y', function(d) { return d.y })
+		  //~ self.node.attr('x', function(d) { return d.x })
+			  //~ .attr('y', function(d) { return d.y })
 
-		  self.link.attr('x1', function(d) { return d.source.x })
-			  .attr('y1', function(d) { return d.source.y })
-			  .attr('x2', function(d) { return d.target.x })
-			  .attr('y2', function(d) { return d.target.y })
+		self.link.attr('x1', function(d) { return d.source.x })
+			.attr('y1', function(d) { return d.source.y })
+			.attr('x2', function(d) { return d.target.x })
+			.attr('y2', function(d) { return d.target.y })
 	 }
 
 	/**
@@ -101,18 +104,18 @@ function ForceChart() {
 	this.init = function () {
 		
 		
-		var width = 500
-		var height = 300
+		self.width = 500
+		self.height = 300
 		
 		self.svg = d3.select('#app').html('')
 			.append('svg')
 			  .attr('width', '100%')
 			  .attr('height', '100%')
 			  .attr('preserveAspectRatio', 'xMinYMin')
-			  .attr('viewBox', '0 0 ' + width + ' ' + height)
+			  .attr('viewBox', '0 0 ' + self.width + ' ' + self.height)
 			  .append('g')
 		
-		self.svg.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+		//~ self.svg.attr('transform', 'translate(' + self.width / 2 + ',' + self.height / 2 + ')')
 		
 		self.link = self.svg.append('g').attr('stroke', '#ddd').attr('stroke-width', 1.5).selectAll('.link')
 		
@@ -145,47 +148,64 @@ function ForceChart() {
 	this.update = function (data) {
 		
 		textScale.domain([data.nodes[data.nodes.length-1].count, data.nodes[0].count])
+		r.domain([data.nodes[data.nodes.length-1].count, data.nodes[0].count])
 
 console.log('graph data', data)
 
 		var linkDistance = d3.scaleLinear()
 			.domain([data.maxWeight, data.minWeight])
-			.range([0, 50])
-
-console.log('linkDistance scale', data.maxWeight, data.minWeight)
-console.log(data.links[0])
-
-		var simulation = d3.forceSimulation(data.nodes)
-			.force('charge', d3.forceManyBody().strength(-50))
-			.force('link', d3.forceLink(data.links).distance(function(d) {return linkDistance(d.weight)}))
-			.force('x', d3.forceX())
-			.force('y', d3.forceY())
+			.range([50, 200])
+			
+		var weightScale = d3.scaleLog()
+			.domain(d3.extent(data.links, function (d) { return d.weight }))
+			.range([.1, 1])
+			
+		//~ self.simulation = d3.forceSimulation(data.nodes)
+			//~ .force('charge', d3.forceManyBody().strength(-50))
+			//~ .force('link', d3.forceLink(data.links).distance(function(d) {return linkDistance(d.weight)}))
+			//~ .force('x', d3.forceX())
+			//~ .force('y', d3.forceY())
 			//~ .force('collide', d3.forceCollide(function(d) {return 10 * textScale(d.count)}))
 			//~ .force('center', d3.forceCenter())
 			//~ .alphaTarget(1)
+			//~ .on('tick', ticked)
+			
+		self.simulation = d3.forceSimulation(data.nodes)
+			.force('link', d3.forceLink(data.links).distance(75).strength(function(d) {return weightScale(d.weight)}))
+			.force('charge', d3.forceManyBody().strength(-200))
+			.force('center', d3.forceCenter(self.width / 2, self.height / 2))
 			.on('tick', ticked)
 
 		// Apply the general update pattern to the nodes.
-		self.node = self.node.data(data.nodes, function(d) { return d.name})
+		self.node = self.node.data(data.nodes, function(d) { return d.name}).enter().append('g')
+			  .call(d3.drag()
+				  .on("start", dragstarted)
+				  .on("drag", dragged)
+				  .on("end", dragended))
 
-		//~ self.node = self.node.enter().append('circle')
-		  //~ .attr('fill', function(d) { return color(d.name) })
+
+		self.node.append('circle')
+		  .attr('fill', function(d) { return color(d.name) })
+		  .attr('r', function(d) { return r(d.count)})
+		  
+		self.node.append('text')
+		  .text(function(d) { return d.name})
+		  
 		  //~ .call(function(node) { node.transition().attr('r', function(d) { return r(d.count)}) })
 		//~ .merge(self.node)
 
-		self.node = self.node.enter()
-		  .append('text')
-		  .attr('transform', function(d) { return 'scale(' + textScale(d.count) + ')'})
-		  .text(function(d) { return d.name})
-		  //~ .attr('fill', function(d) { return color(d.name) })
-		  //~ .call(function(node) { node.transition().attr('r', function(d) { return r(d.count)}) })
-		.merge(self.node)
+		//~ self.node = self.node.enter()
+		  //~ .append('text')
+		  //~ .attr('transform', function(d) { return 'scale(' + textScale(d.count) + ')'})
+		  //~ .text(function(d) { return d.name})
+		//~ .merge(self.node)
 
 		
 		// Apply the general update pattern to the links.
 		self.link = self.link.data(data.links, function(d) { 
-			if ((d.target.name === 'svg' || d.target.name === 'javascript') && d.source.name === 'd3.js')
-				console.log(d, d.weight, linkDistance(d.weight))
+//~ if ((d.target.name === 'google-analytics-api') && d.source.name === 'google-analytics')
+if (['google-analytics', 'google-analytics-api'].indexOf(d.target.name) !== -1 || ['google-analytics', 'google-analytics-api'].indexOf(d.source.name) !== -1)
+	console.log(d.target.name, d.source.name, d.weight)
 			return d.source.name + '-' + d.target.name
 		})
 //~ 
@@ -199,14 +219,15 @@ console.log(data.links[0])
 		  //~ .remove()
 
 		self.link = self.link.enter().append('line')
+			.attr('stroke-opacity', function(d) { return weightScale(d.weight)})
 		  //~ .call(function(link) { link.transition().attr('stroke-opacity', 1) })
 		.merge(self.link)
 
 		// Update and restart the simulation.
-		//~ simulation.nodes(nodes)
-		simulation.nodes(data.nodes)
-		simulation.force('link').links(data.links)
-		simulation.alpha(1).restart()
+		//~ self.simulation.nodes(nodes)
+		self.simulation.nodes(data.nodes)
+		self.simulation.force('link').links(data.links)
+		self.simulation.alpha(1).restart()
 		
 		
 		//~ self.svg.selectAll('.tag').data(data.nodes)
@@ -219,6 +240,23 @@ console.log(data.links[0])
 
 	}
 
+
+	function dragstarted(d) {
+	  if (!d3.event.active) self.simulation.alphaTarget(0.3).restart();
+	  d.fx = d.x;
+	  d.fy = d.y;
+	}
+
+	function dragged(d) {
+	  d.fx = d3.event.x;
+	  d.fy = d3.event.y;
+	}
+
+	function dragended(d) {
+	  if (!d3.event.active) self.simulation.alphaTarget(0);
+	  d.fx = null;
+	  d.fy = null;
+	}
 }
 
 module.exports = ForceChart
