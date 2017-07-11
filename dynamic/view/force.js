df1b7f1015ee7cb3248c68b31ed5dcd4bd7e8dc0
Mehdi El Fadil <m.ef@mango-is.com>
2017-07-11 17:40:53 +0200

    fixed #103

--- dynamic/view/force.js
+++ dynamic/view/force.js
@@ -53,33 +53,176 @@ function ForceChart() {
 		d3.select('#links')
 		  .transition()
 		    .style('opacity', 1)
+		    
+		// avoid overlapping labels
+		relax(self.node)
 		
 	 }
 
 	function dragstarted(d) {
 		
-	  if (!d3.event.active) {
-		  
-		  self.simulation.alphaTarget(0.3).restart()
+		if (!d3.event.active) {
+			
+			self.simulation.alphaTarget(0.3).restart()
 		
 			d3.select('#links')
-			  .transition()
+				.transition()
 				.style('opacity', 0) // hide links when the animation restarts
-	  }
-	  d.fx = d.x;
-	  d.fy = d.y;
+		}
+		d.fx = d.x
+		d.fy = d.y
 	}
 
 	function dragged(d) {
-	  d.fx = d3.event.x;
-	  d.fy = d3.event.y;
+		d.fx = d3.event.x
+		d.fy = d3.event.y
 	}
 
 	function dragended(d) {
-	  if (!d3.event.active) self.simulation.alphaTarget(0);
-	  d.fx = null;
-	  d.fy = null;
+		if (!d3.event.active)
+			self.simulation.alphaTarget(0)
+		d.fx = null
+		d.fy = null
 	}
+
+
+	/**
+	 * relax position of text labels, whenever they are overlapping.
+	 * 
+	 * @param {object} text Labels d3 selection
+	 * 
+	 * @private
+	 * 
+	 */
+	function relax(textLabels) {
+		// relax the position of overlapping labels
+		// only vertical position is modified
+		// based on https://blog.safaribooksonline.com/2014/03/11/solving-d3-label-placement-constraint-relaxing/
+
+		var alpha = 0.5
+			, spacing = 6
+			, again = false
+
+		textLabels.each(function (d, i) {
+			a = this
+			da = d3.select(a)
+			//~ y1 = da.attr("y")
+			
+			var daTransform = getTransformation(da.attr('transform'))
+			
+			y1 = daTransform.translateY
+			textLabels.each(function (d, j) {
+				b = this
+				// a & b are the same element and don't collide.
+				if (a == b) return
+				
+				db = d3.select(b)
+				// Now let's calculate the distance between
+				// these elements. 
+				//~ y2 = db.attr("y")
+				var dbTransform = getTransformation(db.attr('transform'))
+				y2 = dbTransform.translateY
+				
+				deltaY = y1 - y2
+				
+				if (Math.abs(deltaY) > spacing)
+					// Our spacing is greater than our specified spacing,
+					// so they don't collide.
+					return
+
+				if (!overlap ( a, b)) return
+				
+				// If the labels collide, we'll push each 
+				// of the two labels up and down a little bit.
+				again = true
+				sign = deltaY > 0 ? 1 : -1
+				adjust = sign * alpha
+				
+				da.attr("transform", 'translate(' + daTransform.translateX + ',' + (y1 + adjust) + ')')
+
+				db.attr("transform", 'translate(' + dbTransform.translateX + ',' + (y2 - adjust) + ')')
+			})
+		})
+		// Adjust our line leaders here
+		// so that they follow the labels. 
+		if(again) {
+			setTimeout(function() {relax(textLabels)},20)
+		}
+	}
+
+	function overlap (a, b) {
+		// Check whether the bounding box of two texts do overlap.
+		// inspired by http://www.geeksforgeeks.org/find-two-rectangles-overlap/
+
+		var aBbox = a.getBoundingClientRect()
+			, bBbox = b.getBoundingClientRect()
+			, aPoints = {
+				l: {
+					x: aBbox.x
+					, y: aBbox.y
+				}
+				, r: {
+					x: aBbox.x + aBbox.width
+					, y: aBbox.y + aBbox.height
+				}
+			}
+			, bPoints = {
+				l: {
+					x: bBbox.x
+					, y: bBbox.y
+				}
+				, r: {
+					x: bBbox.x + bBbox.width
+					, y: bBbox.y + bBbox.height
+				}
+			}
+		
+		// Check whether one rectangle is on left side of other
+		if (aPoints.l.x > bPoints.r.x || bPoints.l.x > aPoints.r.x)
+			return false
+		
+		// Check whether one rectangle is on above the other
+		if (aPoints.l.y > bPoints.r.y || bPoints.l.y > aPoints.r.y)
+			return false
+			
+		return true
+		
+	}
+	
+	// as in http://stackoverflow.com/a/38230545/1006854
+	function getTransformation(transform) {
+	  // Create a dummy g for calculation purposes only. This will never
+	  // be appended to the DOM and will be discarded once this function 
+	  // returns.
+	  var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+	  
+	  // Set the transform attribute to the provided string value.
+	  g.setAttributeNS(null, "transform", transform);
+	  
+	  // consolidate the SVGTransformList containing all transformations
+	  // to a single SVGTransform of type SVG_TRANSFORM_MATRIX and get
+	  // its SVGMatrix. 
+	  var matrix = g.transform.baseVal.consolidate().matrix;
+	  
+	  // Below calculations are taken and adapted from the private function
+	  // transform/decompose.js of D3's module d3-interpolate.
+	  var {a, b, c, d, e, f} = matrix;   // ES6, if this doesn't work, use below assignment
+	  // var a=matrix.a, b=matrix.b, c=matrix.c, d=matrix.d, e=matrix.e, f=matrix.f; // ES5
+	  var scaleX, scaleY, skewX;
+	  if (scaleX = Math.sqrt(a * a + b * b)) a /= scaleX, b /= scaleX;
+	  if (skewX = a * c + b * d) c -= a * skewX, d -= b * skewX;
+	  if (scaleY = Math.sqrt(c * c + d * d)) c /= scaleY, d /= scaleY, skewX /= scaleY;
+	  if (a * d < b * c) a = -a, b = -b, skewX = -skewX, scaleX = -scaleX;
+	  return {
+		translateX: e,
+		translateY: f,
+		rotate: Math.atan2(b, a) * 180 / Math.PI,
+		skewX: Math.atan(skewX) * 180 / Math.PI,
+		scaleX: scaleX,
+		scaleY: scaleY
+	  };
+	}
+
 
 	/****************************************
 	 *
@@ -140,100 +283,8 @@ function ForceChart() {
 			.force('link', d3.forceLink(data.links).distance(75).strength(function(d) {return self.weightScale(d.weight)}))
 			.force('charge', d3.forceManyBody().strength(-200))
 			.force('center', d3.forceCenter(self.width / 2, self.height / 2))
-			.force('collide', d3.forceCollide(function(d) {return 2 * (textScale(d.count) + d.name.length) }))
 			.on('tick', ticked)
 			.on('end', ended)
-
-// TODO prevent labels from overlapping.
-// cf technique below used in Tribalytics
-// or maybe this one https://stackoverflow.com/questions/17425268/d3js-automatic-labels-placement-to-avoid-overlaps-force-repulsion
-
-// trigger relax function on force simulation end... simulation.on('end', relax)
-
-	//~ function relax(textLabels) {
-		//~ // relax the position of overlapping labels
-		//~ // only vertical position is modified
-		//~ // based on https://blog.safaribooksonline.com/2014/03/11/solving-d3-label-placement-constraint-relaxing/
-//~ 
-		//~ var alpha = 0.5
-			//~ , spacing = 12
-			//~ , again = false;
-//~ 
-		//~ textLabels.each(function (d, i) {
-			//~ a = this;
-			//~ da = d3.select(a);
-			//~ y1 = da.attr("y");
-			//~ textLabels.each(function (d, j) {
-				//~ b = this;
-				//~ // a & b are the same element and don't collide.
-				//~ if (a == b) return;
-				//~ db = d3.select(b);
-				//~ // Now let's calculate the distance between
-				//~ // these elements. 
-				//~ y2 = db.attr("y");
-				//~ deltaY = y1 - y2;
-				//~ 
-				//~ // Our spacing is greater than our specified spacing,
-				//~ // so they don't collide.
-//~ 
-				//~ if (!overlap ( a, b)) return;
-				//~ 
-				//~ // If the labels collide, we'll push each 
-				//~ // of the two labels up and down a little bit.
-				//~ again = true;
-				//~ sign = deltaY > 0 ? 1 : -1;
-				//~ adjust = sign * alpha;
-				//~ da.attr("y",+y1 + adjust);
-				//~ da.selectAll('tspan').attr("y",+y1 + adjust);
-				//~ db.attr("y",+y2 - adjust);
-				//~ db.selectAll('tspan').attr("y",+y2 - adjust);
-			//~ });
-		//~ });
-		//~ // Adjust our line leaders here
-		//~ // so that they follow the labels. 
-		//~ if(again) {
-			//~ setTimeout(function() {relax(textLabels)},20)
-		//~ }
-	//~ }
-//~ 
-	//~ function overlap (a, b) {
-		//~ // Check whether the bounding box of two texts do overlap.
-		//~ // inspired by http://www.geeksforgeeks.org/find-two-rectangles-overlap/
-//~ 
-		//~ var aBbox = a.getBBox()
-			//~ , bBbox = b.getBBox()
-			//~ , aPoints = {
-				//~ l: {
-					//~ x: aBbox.x
-					//~ , y: aBbox.y
-				//~ }
-				//~ , r: {
-					//~ x: aBbox.x + aBbox.width
-					//~ , y: aBbox.y + aBbox.height
-				//~ }
-			//~ }
-			//~ , bPoints = {
-				//~ l: {
-					//~ x: bBbox.x
-					//~ , y: bBbox.y
-				//~ }
-				//~ , r: {
-					//~ x: bBbox.x + bBbox.width
-					//~ , y: bBbox.y + bBbox.height
-				//~ }
-			//~ }
-		//~ 
-		//~ // Check whether one rectangle is on left side of other
-		//~ if (aPoints.l.x > bPoints.r.x || bPoints.l.x > aPoints.r.x)
-			//~ return false
-		//~ 
-		//~ // Check whether one rectangle is on above the other
-		//~ if (aPoints.l.y > bPoints.r.y || bPoints.l.y > aPoints.r.y)
-			//~ return false
-			//~ 
-		//~ return true
-		//~ 
-	//~ }
 
 		// Apply the general update pattern to the nodes.
 		self.node = self.node.data(data.nodes, function(d) { return d.name}).enter().append('g')
