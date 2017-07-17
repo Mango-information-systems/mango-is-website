906b5438c6e90ad5fda48a81634a6e7664dab7ad
Mehdi El Fadil <m.ef@mango-is.com>
2017-07-17 15:32:36 +0200

    fixed #109

--- bin/build-css.sh
+++ bin/build-css.sh
@@ -1,2 +1,3 @@
-cp -r node_modules/font-awesome/fonts source/css/font-awesome/ \
+cp node_modules/normalize.css/normalize.css source/css/ \
+&& cp -r node_modules/font-awesome/fonts source/css/font-awesome/ \
 && cp -r node_modules/font-awesome/css source/css/font-awesome/
