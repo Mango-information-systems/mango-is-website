7c60578cea54c416cea1f933d066d2149ffe15c7
Mehdi El Fadil <m.ef@mango-is.com>
2017-08-04 11:19:58 +0200

    continued user information panel #122 added ejs template

--- dynamic/view/SEUser.js
+++ dynamic/view/SEUser.js
@@ -1,3 +1,6 @@
+var fs = require('fs')
+	, ejs = require('ejs')
+	
 /**
 * Display basic StackExchange user info
 *
@@ -6,24 +9,20 @@
 * 
 */
 function render(target, user) {
+
+	var template = ejs.compile(fs.readFileSync(__dirname + '/../../themes/mango-information-systems/layout/_partial/SE-user-panel.ejs', 'utf-8'))
+
+	user.creationDate = new Date(user.creation_date * 1000)
 	
-	var creationDate = new Date(user.creation_date * 1000)
-		, goldBadge = user.badge_counts.gold? '<span title="' + user.badge_counts.gold + ' gold badges"><i class="fa fa-circle" aria-hidden="true" style="color:#FFCC01;"></i> ' + user.badge_counts.gold + '</span> ': ''
-		, silverBadge = user.badge_counts.silver? '<span title="' + user.badge_counts.silver + ' silver badges"><i class="fa fa-circle" aria-hidden="true" style="color:#B4B8BC;"></i> ' + user.badge_counts.silver + '</span> ': ''
-		, bronzeBadge = user.badge_counts.bronze? '<span title="' + user.badge_counts.bronze + ' bronze badges"><i class="fa fa-circle" aria-hidden="true" style="color:#D1A684;"></i> ' + user.badge_counts.bronze + '</span> ': ''
+	user.goldBadge = user.badge_counts.gold? '<span title="' + user.badge_counts.gold + ' gold badges"><i class="fa fa-circle" aria-hidden="true" style="color:#FFCC01;"></i> ' + user.badge_counts.gold + '</span> ': ''
 	
-	target.html('<div class="row">'
-		 + '<h5><a href="' + user.link + '">' + user.display_name + '</a></h5>'
-		+ '<p>' + user.location + '</p>'
-		+ '<p>member since ' + creationDate.toLocaleString('en-us', { month: 'short' }) + ' ' + creationDate.getFullYear() + '</p>'
-		+ '</div>'
-		+ '<p>'
-		+ goldBadge
-		+ silverBadge
-		+ bronzeBadge
-		+ '<p class="lead">' + user.reputation + '</p>'
+	user.silverBadge = user.badge_counts.silver? '<span title="' + user.badge_counts.silver + ' silver badges"><i class="fa fa-circle" aria-hidden="true" style="color:#B4B8BC;"></i> ' + user.badge_counts.silver + '</span> ': ''
 		
-	)
+	user.bronzeBadge = user.badge_counts.bronze? '<span title="' + user.badge_counts.bronze + ' bronze badges"><i class="fa fa-circle" aria-hidden="true" style="color:#D1A684;"></i> ' + user.badge_counts.bronze + '</span> ': ''
+
+
+	target.html(template(user))
+
 }
 
 module.exports = {
