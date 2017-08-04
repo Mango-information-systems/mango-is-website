217840af7d4fa08914e3adc9f03405853ca65e00
Mehdi El Fadil <m.ef@mango-is.com>
2017-08-04 12:24:41 +0200

    user panel layout #122

--- dynamic/view/SEUser.js
+++ dynamic/view/SEUser.js
@@ -13,13 +13,6 @@ function render(target, user) {
 	var template = ejs.compile(fs.readFileSync(__dirname + '/../../themes/mango-information-systems/layout/_partial/SE-user-panel.ejs', 'utf-8'))
 
 	user.creationDate = new Date(user.creation_date * 1000)
-	
-	user.goldBadge = user.badge_counts.gold? '<span title="' + user.badge_counts.gold + ' gold badges"><i class="fa fa-circle" aria-hidden="true" style="color:#FFCC01;"></i> ' + user.badge_counts.gold + '</span> ': ''
-	
-	user.silverBadge = user.badge_counts.silver? '<span title="' + user.badge_counts.silver + ' silver badges"><i class="fa fa-circle" aria-hidden="true" style="color:#B4B8BC;"></i> ' + user.badge_counts.silver + '</span> ': ''
-		
-	user.bronzeBadge = user.badge_counts.bronze? '<span title="' + user.badge_counts.bronze + ' bronze badges"><i class="fa fa-circle" aria-hidden="true" style="color:#D1A684;"></i> ' + user.badge_counts.bronze + '</span> ': ''
-
 
 	target.html(template(user))
 
