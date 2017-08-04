d9b800027385c782c67d5b6854097f1f5738fcb2
Mehdi El Fadil <m.ef@mango-is.com>
2017-08-04 14:21:43 +0200

    DRY #122 created new view for app skull

--- dynamic/myTagOverflow.js
+++ dynamic/myTagOverflow.js
@@ -8,8 +8,9 @@ var d3 = require('d3')
 	, app = {
 		controller: {}
 		, view: {
-			signIn: require('./view/sign-in-with-stackExchange')
+			myTagOverflow: require('./view/myTagOverflow')
 			, chart: new Force()
+			, signIn: require('./view/sign-in-with-stackExchange')
 			, userInfo: require('./view/SEUser')
 			, warningMessage: require('./view/warning-message')
 		}
@@ -20,6 +21,8 @@ var appContainer = d3.select('#app')
 	, dashboardBody
 	, refreshInterval
 	, chartInitialized = false
+
+app.view.myTagOverflow.render(appContainer)
 
 // temp
 window.storage = storage
@@ -118,7 +121,7 @@ function initiateExtraction(hasError, opts) {
 					, hasError: hasError !== null
 					, action: function() {
 						
-						appContainer.html('<div id="user" class="xs-twelve lg-two columns"></div><div id="chart" class="xs-twelve lg-ten columns"><?xml version="1.0" encoding="utf-8"?><svg width="120px" height="120px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-ripple"><rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect><g> <animate attributeName="opacity" dur="2s" repeatCount="indefinite" begin="0s" keyTimes="0;0.33;1" values="1;1;0"></animate><circle cx="50" cy="50" r="40" stroke="#565656" fill="none" stroke-width="6" stroke-linecap="round"><animate attributeName="r" dur="2s" repeatCount="indefinite" begin="0s" keyTimes="0;0.33;1" values="0;22;44"></animate></circle></g><g><animate attributeName="opacity" dur="2s" repeatCount="indefinite" begin="1s" keyTimes="0;0.33;1" values="1;1;0"></animate><circle cx="50" cy="50" r="40" stroke="#FF6600" fill="none" stroke-width="6" stroke-linecap="round"><animate attributeName="r" dur="2s" repeatCount="indefinite" begin="1s" keyTimes="0;0.33;1" values="0;22;44"></animate></circle></g></svg><br><small>Loading...</small></div>')
+						app.view.myTagOverflow.render(appContainer)
 						
 						app.controller.stackExchangeApi.signIn(start)
 					}
