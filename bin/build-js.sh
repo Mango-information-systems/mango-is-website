1c73e283d16c1b46146482b664fb158ca850bc3a
Mehdi El Fadil <m.ef@mango-is.com>
2017-08-09 16:54:55 +0200

    updated js build script #121 re. new name

--- bin/build-js.sh
+++ bin/build-js.sh
@@ -1,3 +1,3 @@
 browserify -t uglifyify dynamic/client.js | uglifyjs -c > source/js/site.js
 browserify -t brfs -t uglifyify dynamic/g-realtime.js | uglifyjs -c > source/js/g-realtime.js
-browserify -t brfs -t uglifyify dynamic/stackexchange-stats.js | uglifyjs -c > source/js/stackexchange-stats.js
+browserify -t brfs -t uglifyify dynamic/myTagOverflow.js | uglifyjs -c > source/js/myTagOverflow.js
