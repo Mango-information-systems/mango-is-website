2c2b028e13f197359c79ab9f04863631d83324ce
Mehdi El Fadil <m.ef@mango-is.com>
2017-09-02 15:20:42 +0200

    added myTagOverflow preview image in unsupported browser page + bumped version number #147

--- dynamic/myTagOverflowChecker.js
+++ dynamic/myTagOverflowChecker.js
@@ -7,7 +7,7 @@ checker.loadIfSupportES6('/js/myTagOverflow.js', function() {
 	warningMessage.render({
 			target: document.getElementById('app')
 			, title: 'Browser not supported'
-			, message: '<p>This tool is not compatible with your browser. Please try using Firefox or Chrome instead.</p>'
+			, message: '<p>This tool is not compatible with your browser. Please try using Firefox or Chrome instead.</p>Preview:</p><img class="u-img-responsive" alt="myTagOverflow visualization preview" src="/img/myTagOverflow-demo.jpg"/>'
 		})
 	
 })
