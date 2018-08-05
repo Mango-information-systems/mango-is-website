9e73853ea2ba796366a4622e4ce4bba9385fb012
Mehdi El Fadil <m.ef@mango-is.com>
2018-08-05 22:57:00 +0200

    added thousand separators to reputation score and badges counts #171

--- dynamic/view/SEUser.js
+++ dynamic/view/SEUser.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 	, ejs = require('ejs')
+	, d3 = require('d3')
 	
 /**
 * Display basic StackExchange user info
@@ -13,6 +14,16 @@ function render(target, user) {
 	var template = ejs.compile(fs.readFileSync(__dirname + '/../../themes/mango-information-systems/layout/_partial/SE-user-panel.ejs', 'utf-8'))
 
 	user.creationDate = new Date(user.creation_date * 1000)
+	
+	// format numbers
+	if (typeof user.reputation === 'number')
+		user.reputation = d3.format(',.2d')(user.reputation)
+	
+	Object.keys(user.badge_counts).forEach(function(badge) {
+		if (typeof user.badge_counts[badge] === 'number')
+			user.badge_counts[badge] = d3.format(',.2d')(user.badge_counts[badge])
+	})
+
 
 	target.html(template(user))
 
