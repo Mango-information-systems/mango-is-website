09ef99ec1fdebb51f38ca53482290ecb56e1734c
Mehdi El Fadil <m.ef@mango-is.com>
2023-04-27 13:34:08 +0100

    update build script to include necessary dependency #230

--- bin/build-js.sh
+++ bin/build-js.sh
@@ -3,4 +3,4 @@ browserify -t uglifyify dynamic/client.js | uglifyjs -c > source/legacy/js/site.
 #~browserify -t brfs -t uglifyify dynamic/myTagOverflowChecker.js | uglifyjs -c > source/js/myTagOverflowChecker.js
 #~browserify -t brfs -t uglifyify dynamic/pullReceipt.js | uglifyjs -c > source/js/pullReceipt.js
 #~browserify -t brfs -t uglifyify dynamic/myTagOverflow.js | uglifyjs -c > source/js/myTagOverflow.js
-#~cp node_modules/localforage/dist/localforage.nopromises.min.js source/js/localforage.nopromises.min.js
+cp node_modules/localforage/dist/localforage.nopromises.min.js themes/mango-information-systems/source/legacy/js/localforage.nopromises.min.js
