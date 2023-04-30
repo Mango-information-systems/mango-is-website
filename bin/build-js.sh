8f5b9d71c4b36290db4a8301edcb3df902df285a
Mehdi El Fadil <m.ef@mango-is.com>
2023-04-30 18:14:10 +0100

    start port of myTagOverflow #230

--- bin/build-js.sh
+++ bin/build-js.sh
@@ -1,6 +1,6 @@
 browserify -t uglifyify dynamic/client.js | uglifyjs -c > source/legacy/js/site.js
 #~browserify -t brfs -t uglifyify dynamic/g-realtime.js | uglifyjs -c > source/js/g-realtime.js
-#~browserify -t brfs -t uglifyify dynamic/myTagOverflowChecker.js | uglifyjs -c > source/js/myTagOverflowChecker.js
+browserify -t brfs -t uglifyify dynamic/myTagOverflowChecker.js | uglifyjs -c > themes/mango-information-systems/source/legacy/js/myTagOverflowChecker.js
 #~browserify -t brfs -t uglifyify dynamic/pullReceipt.js | uglifyjs -c > source/js/pullReceipt.js
-#~browserify -t brfs -t uglifyify dynamic/myTagOverflow.js | uglifyjs -c > source/js/myTagOverflow.js
+browserify -t brfs -t uglifyify dynamic/myTagOverflow.js | uglifyjs -c > themes/mango-information-systems/source/legacy/js/myTagOverflow.js
 cp node_modules/localforage/dist/localforage.nopromises.min.js themes/mango-information-systems/source/legacy/js/localforage.nopromises.min.js
